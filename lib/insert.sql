09a776cffbc476ec29b01fb30cee8a02a9f421a6
Taishawn King <taishawnking1@yahoo.com>
2020-04-16 17:15:37 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+Insert Into bears(name) values ("Mr. Chocolate");
+Insert Into bears(name) values ("Rowdy");
+Insert Into bears(name) values ('Tabitha');
+Insert Into bears(name) values ('Sergeant Brown');
+Insert Into bears(name) values ('Melissa');
+Insert Into bears(name) values ('Grinch');
+Insert Into bears(name) values ('Wendy'); 
+Insert Into bears(name) values (null);
